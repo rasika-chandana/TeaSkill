c81fbbe1d71d8396dcac6e028b4d87ebef063001
Rasika Karunapala <rasikachandana.karunapala@mastercard.com>
2018-05-13 18:32:30 +0800

    Added tea skill

---
+++ __init__.py
@@ -0,0 +1,65 @@
+# TODO: Add an appropriate license to your skill before publishing.  See
+# the LICENSE file for more information.
+
+# Below is the list of outside modules you'll be using in your skill.
+# They might be built-in to Python, from mycroft-core or from external
+# libraries.  If you use an external library, be sure to include it
+# in the requirements.txt file so the library is installed properly
+# when the skill gets installed later by a user.
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.context import adds_context, removes_context
+from mycroft.util.log import LOG
+
+# Each skill is contained within its own class, which inherits base methods
+# from the MycroftSkill class.  You extend this class as shown below.
+
+# TODO: Change "Template" to a unique name for your skill
+class TeaSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(TeaSkill, self).__init__(name="TeaSkill")
+
+
+    @intent_handler(IntentBuilder('TeaIntent').require("TeaKeyword"))
+    @adds_context('MilkContext')
+    def handle_tea_intent(self, message):
+        self.milk = False
+        self.speak('Of course, would you like Milk with that?', expect_response=True)
+
+
+    @intent_handler(IntentBuilder('NoMilkIntent').require("NoKeyword").require('MilkContext').build())
+    @removes_context('MilkContext')
+    @adds_context('HoneyContext')
+    def handle_yes_milk_intent(self, message):
+        self.milk = True
+        self.speak('all right, any Honey?', expect_response=True)
+
+    @intent_handler(IntentBuilder('YesMilkIntent').require("YesKeyword").require('MilkContext').build())
+    def handle_no_milk_intent(self, message):
+        self.speak('What about Honey?', expect_response=True)
+
+    @intent_handler(IntentBuilder('NoHoneyIntent').require("NoKeyword").require('HoneyContext').build())
+    @removes_context('HoneyContext')
+    def handle_no_honey_intent(self, message):
+        if self.milk:
+            self.speak('Heres your Tea, straight up')
+        else:
+            self.speak('Heres your Tea with a dash of Milk')
+
+
+    @intent_handler(IntentBuilder('YesHoneyIntent').require("YesKeyword").require('HoneyContext').build())
+    @removes_context('HoneyContext')
+    def handle_yes_honey_intent(self, message):
+        if self.milk:
+            self.speak('Heres your Tea with Honey')
+        else:
+            self.speak('Heres your Tea with Milk and Honey')
+
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+def create_skill():
+    return TeaSkill()
