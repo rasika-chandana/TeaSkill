c9582b9ff611de4fb07bf12aff751151cd3de714
Rasika Karunapala <rasikachandana.karunapala@mastercard.com>
2018-05-13 22:31:27 +0800

    Added tea skill

--- __init__.py
+++ __init__.py
@@ -24,21 +24,18 @@ class TeaSkill(MycroftSkill):
 
 
     @intent_handler(IntentBuilder('TeaIntent').require("TeaKeyword"))
-    # @adds_context('MilkContext')
+    @adds_context('MilkContext')
     def handle_tea_intent(self, message):
         self.milk = False
         self.speak('Of course, would you like Milk with that?', expect_response=True)
-        self.set_context('MilkContext')
 
 
     @intent_handler(IntentBuilder('NoMilkIntent').require("NoKeyword").require('MilkContext').build())
-    # @removes_context('MilkContext')
-    # @adds_context('HoneyContext')
+    @removes_context('MilkContext')
+    @adds_context('HoneyContext')
     def handle_yes_milk_intent(self, message):
         self.milk = True
         self.speak('all right, any Honey?', expect_response=True)
-        self.remove_context('MilkContext')
-        self.set_context('HoneyContext')
 
     @intent_handler(IntentBuilder('YesMilkIntent').require("YesKeyword").require('MilkContext').build())
     def handle_no_milk_intent(self, message):
