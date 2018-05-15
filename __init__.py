7901e377ef3667bcf3d9cef2e4e806fb244f0aeb
Rasika Karunapala <rasikachandana.karunapala@mastercard.com>
2018-05-15 13:07:28 +0800

    Added tea skill

--- __init__.py
+++ __init__.py
@@ -33,12 +33,12 @@ class AssistMcSkill(MycroftSkill):
 		self.milk = False
 		self.speak_dialog('order.food.confirm.reply', expect_response=True)
 
-	@intent_handler(IntentBuilder('YesMilkIntent').require("YesKeyword").require('OrderFoodContext').build())
+	@intent_handler(IntentBuilder('YesOrderFoodIntent').require("YesKeyword").require('OrderFoodContext').build())
 	@adds_context('HoneyContext')
 	def handle_no_milk_intent(self, message):
 		self.speak('from where do you want to order', expect_response=True)
 
-	@intent_handler(IntentBuilder('YesMilkIntent').require("NoKeyword").require('OrderFoodContext').build())
+	@intent_handler(IntentBuilder('NoOrderFoodIntent').require("NoKeyword").require('OrderFoodContext').build())
 	@adds_context('HoneyContext')
 	def handle_no_milk_intent(self, message):
 		self.speak('Ok, Tell me when you are hungry', expect_response=True)
@@ -49,57 +49,57 @@ class AssistMcSkill(MycroftSkill):
 
 
 
-	@intent_handler(IntentBuilder('UnhandledTeaIntent').require('MilkContext').optionally("Unhandled").build())
-	def handle_tea_unhandled_intent(self, message):
-		self.speak('Oops, I didn\'t get that', expect_response=True)
-
-	@intent_handler(IntentBuilder('NoMilkIntent').require("NoKeyword").require('MilkContext').build())
-	@adds_context('HoneyContext')
-	def handle_yes_milk_intent(self, message):
-		self.milk = True
-		self.speak('all right, any Honey?', expect_response=True)
-
-	@intent_handler(IntentBuilder('YesMilkIntent').require("YesKeyword").require('MilkContext').build())
-	@adds_context('HoneyContext')
-	def handle_no_milk_intent(self, message):
-		self.speak('What about Honey?', expect_response=True)
-
-	@intent_handler(IntentBuilder('NoHoneyIntent').require("NoKeyword").require('HoneyContext').build())
-	@removes_context('HoneyContext')
-	def handle_no_honey_intent(self, message):
-		self.send_order()
-
-	@intent_handler(IntentBuilder('YesHoneyIntent').require("YesKeyword").require('HoneyContext').build())
-	@removes_context('HoneyContext')
-	def handle_yes_honey_intent(self, message):
-		self.send_order()
-
-	@intent_handler(IntentBuilder('UnhandledTeaIntent').require('HoneyContext').optionally("Unhandled").build())
-	def handle_honey_unhandled_intent(self, message):
-		self.speak('Oops, I didn\'t get that', expect_response=True)
-
-	def send_order(self):
-		try:
-			data = {
-				'orders': [{
-					'menu_id': 1,
-					'menu_title': 'Coffee',
-					'quantity': 1
-				}, {
-					'menu_id': 2,
-					'menu_title': 'Tea',
-					'quantity': 1
-				},
-				]
-			}
-			response = requests.post('{}/api/qkr/submit/order/abc'.format(API_URL), json=data)
-			result_str = response.text
-			result = json.loads(result_str)
-
-			self.speak('Thank you, your order is being prepared. \
-				Payment will be done on your default card. Please authenticate on your mobile app to continue.')
-		except:
-			self.speak('Sorry, unable to send order at this time.')
+	# @intent_handler(IntentBuilder('UnhandledTeaIntent').require('MilkContext').optionally("Unhandled").build())
+	# def handle_tea_unhandled_intent(self, message):
+	# 	self.speak('Oops, I didn\'t get that', expect_response=True)
+    #
+	# @intent_handler(IntentBuilder('NoMilkIntent').require("NoKeyword").require('MilkContext').build())
+	# @adds_context('HoneyContext')
+	# def handle_yes_milk_intent(self, message):
+	# 	self.milk = True
+	# 	self.speak('all right, any Honey?', expect_response=True)
+    #
+	# @intent_handler(IntentBuilder('YesMilkIntent').require("YesKeyword").require('MilkContext').build())
+	# @adds_context('HoneyContext')
+	# def handle_no_milk_intent(self, message):
+	# 	self.speak('What about Honey?', expect_response=True)
+    #
+	# @intent_handler(IntentBuilder('NoHoneyIntent').require("NoKeyword").require('HoneyContext').build())
+	# @removes_context('HoneyContext')
+	# def handle_no_honey_intent(self, message):
+	# 	self.send_order()
+    #
+	# @intent_handler(IntentBuilder('YesHoneyIntent').require("YesKeyword").require('HoneyContext').build())
+	# @removes_context('HoneyContext')
+	# def handle_yes_honey_intent(self, message):
+	# 	self.send_order()
+    #
+	# @intent_handler(IntentBuilder('UnhandledTeaIntent').require('HoneyContext').optionally("Unhandled").build())
+	# def handle_honey_unhandled_intent(self, message):
+	# 	self.speak('Oops, I didn\'t get that', expect_response=True)
+    #
+	# def send_order(self):
+	# 	try:
+	# 		data = {
+	# 			'orders': [{
+	# 				'menu_id': 1,
+	# 				'menu_title': 'Coffee',
+	# 				'quantity': 1
+	# 			}, {
+	# 				'menu_id': 2,
+	# 				'menu_title': 'Tea',
+	# 				'quantity': 1
+	# 			},
+	# 			]
+	# 		}
+	# 		response = requests.post('{}/api/qkr/submit/order/abc'.format(API_URL), json=data)
+	# 		result_str = response.text
+	# 		result = json.loads(result_str)
+    #
+	# 		self.speak('Thank you, your order is being prepared. \
+	# 			Payment will be done on your default card. Please authenticate on your mobile app to continue.')
+	# 	except:
+	# 		self.speak('Sorry, unable to send order at this time.')
 
 
 # The "create_skill()" method is used to create an instance of the skill.
